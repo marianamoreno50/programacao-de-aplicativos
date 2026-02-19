9220a2ad825b2d00281d6df0f2fba278ab1dc909
Mariana Moreno <m0ren0.m4ry@gmail.com>
2026-02-19 10:04:29 -0300

    Primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,9 @@
+idade = 16 # variável inteira
+peso = 56.6 # variável float 
+nome = "Mariana Ramos Moreno"
+sexo_feminino = True # variável bool
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo_feminino)
