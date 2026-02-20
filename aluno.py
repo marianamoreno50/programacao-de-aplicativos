b527c0dd17a501139c459348b5558fd5801e21ea
mariana <m0ren0.m4ry@gmail.com>
2026-02-20 09:54:17 -0300

    oi gente

---
+++ aluno.py
@@ -0,0 +1,9 @@
+nome = input("digite o seu nome")
+telefone = int(input("digite seu telefone"))
+idade = int(input("digite sua idade"))
+cidade = input("digite sua cidade")
+
+print("oi" , nome)
+print("seu telefone é" , telefone)
+print("sua idade é" , idade)
+print("sua cidade é" , cidade)
---
+++ matematica.py
@@ -0,0 +1,13 @@
+valor1 = int(input("digite o primeiro valor"))
+valor2 = int(input("digite o segundo valor"))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+divisao = valor1 / valor2
+multiplicacao = valor1 * valor2
+
+
+print ("o resultado da soma é" , soma)
+print ("o resultado da subtracao é" , subtracao)
+print ("o resultado da divisao é" , divisao)
+print ("o resultado da multiplicacao é" , multiplicacao)
