7fb5d2d2ce79db96cd0549ac0feb4a09d4a97d3d
mariana <m0ren0.m4ry@gmail.com>
2026-03-11 12:08:47 -0300

    atividades 11/03

--- distribuicao.py
+++ distribuicao.py
@@ -2,4 +2,8 @@ codigo = int(input("coloque o codigo do pacote: "))
 peso = float(input("digite o peso: "))
 
 if peso < 5 and(codigo  % 10 == 0):
-    print(f"pacote {codigo}: {}")
+    print(f"pacote {codigo}: ENTREGA EXPRESSA")
+elif peso > 50:
+    print(f"pacote {codigo} CARGA PESADA")
+else:
+    print("INFORMAÇÕES INVALIDAS!")
