72b6c1efceefda9c3c9734615dc176dd07fbdc08
mariana <m0ren0.m4ry@gmail.com>
2026-03-23 12:42:56 -0300

    23/03

---
+++ alinhamento.py
@@ -0,0 +1,8 @@
+fila = ["Ana", "Bruno", "Caio"]
+fila.append ("Daniel")
+print ("fila atualizada", fila)
+
+fila.insert (0, "zuleica")
+print ("fila atualizada novamente", fila)
+pessoas = len (fila)
+print (f"{pessoas} peeoas estao na fila")
---
+++ atualizacao.precos.py
@@ -0,0 +1,4 @@
+precos = [19.90, 45.00, 89.90, 120.00]
+55.50 [0]
+89.90 / 2 [2]
+print (f"osnovos precos sao: {precos}")
